a3615cb56618e8049c256257b2c55c1d1ec19e80
eslota1 <elisabethslota0411@gmail.com>
2021-03-09 23:23:20 -0500

    committing rest of files

---
+++ src/main/java/com/example/springboot/PostHistoryController.java
@@ -0,0 +1,71 @@
+package com.example.springboot;
+
+import com.example.utils.PostLogger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PostHistoryController {
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/api")
+    public String postString(@RequestParam("post_input_text") String inputTest,
+                                     Model model) {
+        model.addAttribute("title", "Post Page");
+        System.out.println(inputTest);
+        String pathToLoggFile = env.getProperty("post.log.file");
+        System.out.println(pathToLoggFile);
+        PostLogger pl = new PostLogger(pathToLoggFile);
+        pl.writeLine(inputTest);
+        return "index";
+    }
+
+
+    @RequestMapping(value = "/history")
+    public String getHistoryString(Model model) {
+        String pathToLoggFile = env.getProperty("post.log.file");
+        PostLogger pl = new PostLogger(pathToLoggFile);
+        String history = pl.readHistory();
+        model.addAttribute("history", history);
+        model.addAttribute("newLineChar", '\n');
+        return "history";
+    }
+
+    @RequestMapping(value = "/delete")
+    public String deletePostString(@RequestParam("post_text") String deleteText, Model model) {
+        model.addAttribute("title", "Delete Page");
+        System.out.println(deleteText);
+        String pathToLoggFile = env.getProperty("post.log.file");
+        PostLogger pl = new PostLogger(pathToLoggFile);
+        boolean deleted = pl.deleteInputText(deleteText);
+        model.addAttribute("deleted", deleted);
+        if (!deleteText.isEmpty()) model.addAttribute("deleteAttempted", true);
+        System.out.println("deleted: " + deleted);
+        return "delete";
+    }
+
+    @RequestMapping(value = "/search")
+    public String searchPostString(@RequestParam("posts_text") String searchText, Model model) {
+        model.addAttribute("title", "Search Page");
+        System.out.println(searchText);
+        String pathToLoggFile = env.getProperty("post.log.file");
+        PostLogger pl = new PostLogger(pathToLoggFile);
+        String foundline = pl.searchInputText(searchText);
+        model.addAttribute("foundline", foundline);
+        if (!searchText.isEmpty()) model.addAttribute("searchAttempted", true);
+        return "search";
+    }
+}
+
---
+++ src/main/java/com/example/utils/PostLogger.java
@@ -0,0 +1,136 @@
+package com.example.utils;
+
+import java.io.*;
+import java.util.regex.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+
+public class PostLogger {
+    private String filePath;
+
+    public PostLogger(String path){
+        this.filePath = path;
+        File logFile = new File(path);
+        try {
+            if (logFile.createNewFile()) {
+                System.out.println("Log file created: " + logFile.getName());
+            } else {
+                System.out.println("Log file already exists.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeLine(String line){
+        Path p = Paths.get(filePath);
+        String s = System.lineSeparator() + line;
+        try (OutputStream out = new BufferedOutputStream(Files.newOutputStream(p,
+                StandardOpenOption.APPEND))) {
+            out.write(s.getBytes());
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    public String readHistory(){
+        String content = "";
+        ArrayDeque<String> stack = new ArrayDeque<String>();
+        try {
+            File file = new File(filePath);
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            //read the file and add only not an empty string to the stack
+            String line = reader.readLine();
+            while (line != null) {
+                if (line.trim().length() > 0)
+                    stack.push(line.trim());
+                line = reader.readLine();
+            }
+            reader.close();
+            fr.close();
+            //add posts to the content in inverse order
+            Iterator<String> it = stack.iterator();
+            while (it.hasNext()) {
+                content += it.next() +"\n";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(content);
+        return content;
+    }
+
+    public boolean deleteInputText (String delete){
+        boolean d = false;
+        String fPath = filePath+".temp";
+        try{
+            File file = new File(filePath);
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            File file1 = new File(fPath);
+            FileWriter fw = new FileWriter(file1);
+            BufferedWriter writer = new BufferedWriter(fw);
+            String line = reader.readLine();
+
+            while (line != null){
+                String deleteTrimLn = delete.trim();
+
+                if (line.trim().length() > 0 && !deleteTrimLn.equals(line.trim())){
+                    writer.write(line+System.lineSeparator());
+                }
+                else
+                    d = true;
+
+                line = reader.readLine();
+            }
+            reader.close();
+            writer.close();
+            fr.close();
+            fw.close();
+            return (d && file1.renameTo(file));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public String searchInputText (String searchT) {
+        // String fPath = filePath + ".temp";
+        // Pattern pattern = Pattern.compile(searchT);
+        // System.out.println("Matched Content: ");
+        try {
+            File file = new File(filePath);
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            int linecount = 0;
+
+            while ((line= reader.readLine()) != null) {
+                linecount++;
+                int indexfound;
+                indexfound = line.indexOf(searchT);
+
+                if (indexfound > -1){
+                    sb.append(line);
+                    sb.append("\n");
+                }
+            }
+
+            reader.close();
+            fr.close();
+            return sb.toString();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Nothing found";
+    }
+}
+
